caee46039d17daa950163edf34faa9286140ae1b
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-05-22 23:30:18 +0430

    init commit

---
+++ app/src/main/java/com/fekri/api/RetrofitClient.java
@@ -0,0 +1,42 @@
+package com.fekri.api;
+
+import com.fekri.utility.ConstVar;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static RetrofitClient instance = null;
+    private ApiInterface apiInterface;
+
+    public OkHttpClient getClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        return client;
+    }
+
+    private RetrofitClient(){
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(ConstVar.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(getClient())
+                .build();
+        apiInterface = retrofit.create(ApiInterface.class);
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if (instance == null) {
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+
+    public ApiInterface getApiInterface() {
+        return apiInterface;
+    }
+}
--- app/src/main/java/com/fekri/ui/activity/DetailsActivity.java
+++ app/src/main/java/com/fekri/ui/activity/DetailsActivity.java
@@ -1,4 +1,4 @@
-package com.fekri.ui;
+package com.fekri.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,12 @@ import android.os.Bundle;
 
 import com.fekri.R;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
+
     }
 }
