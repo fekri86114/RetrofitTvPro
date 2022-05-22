3dd8d505fdce42fb5898bd93902a6edcecd71b6c
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-05-22 16:42:17 +0430

    init commit

--- app/src/main/java/com/fekri/api/ApiInterface.java
+++ app/src/main/java/com/fekri/api/ApiInterface.java
@@ -1,5 +1,13 @@
 package com.fekri.api;
 
-public interface ApiInterface {
+import com.fekri.model.TvMazeModelItem;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+    @GET("/shows/1/cast")
+    Call<List<TvMazeModelItem>> getHeroList();
 }
